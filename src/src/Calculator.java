91d56a7798c7349bbc9a22b4f6e10524b9dad3ce
jascha10 <jascha10@wsc.edu>
2025-04-07 09:21:51 -0500

    Added starting code and packages(not sure if packages are correct)

--- src/src/Calculator.java
+++ src/src/Calculator.java
@@ -1,2 +1,19 @@
 public class Calculator {
+    public void updateDisplay{
+        System.out.println();
+        System.out.println(" %,12.2d%n", previousValue);
+        System.out.println("%1s%,12.2d%n", operator, inputvalue);
+        System.out.println("==============");
+        System.out.println(" %,12.2d%n", currentValue);
+        System.out.println();
+    }
+
+    @Override
+    public <N extends Number> void add(N value) {
+        operator ='+';
+        previousValue = currentValue;
+        this.inputValue = value.doubleValue();
+        currentValue += inputValue;
+        updateDisplay();
+    }
 }
