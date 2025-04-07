6a07de166062421e8331fd0b4ff5952aeace5d66
jascha10 <jascha10@wsc.edu>
2025-04-07 09:42:01 -0500

    Reorganized the @override code in the advancecalc class

--- src/src/AdvanceCalc.java
+++ src/src/AdvanceCalc.java
@@ -1,13 +1,6 @@
-public class AdvanceCalc {
+public class AdvanceCalc extends Calculator {
+    int precision = 0;
     public void updateDisplay() {
-        @Override
-        public void sqrt() {
-            operator = '\u221A';
-            previousValue = currentValue;
-            inputValue = 0.0;
-            currentValue = Math.sqrt(this.currentValue);
-            updateDisplay();
-        }
         @Override
         public void updateDisplay() {
             String format1 = " %,12." + precision + "f%n";
@@ -24,7 +17,15 @@ public class AdvanceCalc {
             System.out.println("==============");
             System.out.printf(format1, currentValue);
             System.out.println();
-        }
-    }
 
+        }
+        @Override
+        public void sqrt() {
+            operator = '\u221A';
+            previousValue = currentValue;
+            inputValue = 0.0;
+            currentValue = Math.sqrt(this.currentValue);
+            updateDisplay();
+        }
+        }
 }
