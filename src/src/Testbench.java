7ef8d0f35eadb237a7f088b5ac75bc75a5dce394
ranran21-C <raniahassani2303@gmail.com>
2025-04-08 23:09:41 -0500

    Update Testbench.java so we can test all the methods and at first i did put them inside the calculator

--- src/src/Testbench.java
+++ src/src/Testbench.java
@@ -1,2 +1,12 @@
 public class Testbench {
+    public static void main(String[] args) {
+        System.out.println("Calculator On");
+
+        Calculator calc = new Calculator();
+        calc.clear();
+        calc.add(10.22);
+        calc.subtract(2.22);
+        calc.multiply(10.00);
+        calc.divide(2.00);
+    }
 }
