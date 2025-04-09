2c46615d6074e9319ed7f47c45e14a889107ba06
ranran21-C <raniahassani2303@gmail.com>
2025-04-08 23:02:35 -0500

    Update My Testbench to test subtraction

--- src/src/Calculator.java
+++ src/src/Calculator.java
@@ -34,13 +34,13 @@ s public class Calculator {
     currentValue -= inputValue;
     updateDisplay();
 }
-    public class Testbench {
+   public class Testbench {
     public static void main(String[] args) {
         System.out.println("Calculator On");
 
         Calculator calc = new Calculator();
-        calc.clear();
-        calc.add(10.22);  // Shows output for: + 10.22 = 10.22
+        calc.clear();             
+        calc.add(10.22);          
+        calc.subtract(2.22);       
     }
-
 }
