30ddc1c1f6db5a9755bbb736faf9749b8ff0e35a
ranran21-C <raniahassani2303@gmail.com>
2025-04-08 23:22:02 -0500

    updated so i can test all of them

--- src/src/Testbench.java
+++ src/src/Testbench.java
@@ -2,11 +2,12 @@ public class Testbench {
     public static void main(String[] args) {
         System.out.println("Calculator On");
 
-        Calculator calc = new Calculator();
+        AdvanceCalc calc = new AdvanceCalc();
         calc.clear();
-        calc.add(10.22);
-        calc.subtract(2.22);
-        calc.multiply(10.00);
-        calc.divide(2.00);
+        calc.add(10.22);          // Test normal add
+        calc.setPrecision(4);     // Set precision to 4 decimal places
+        calc.multiply(10.00);     // Test multiply with new precision
+        calc.sqrt(1600.00);       // Test square root with precision
+        calc.memoryAdd();         // Test memory functionality
     }
 }
