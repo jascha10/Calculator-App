afef8f697b6d0e853c2888c356f53a64a750562d
ranran21-C <raniahassani2303@gmail.com>
2025-04-08 23:13:46 -0500

    create memory calc class with memory add

--- src/src/MemoryCalc.java
+++ src/src/MemoryCalc.java
@@ -1,2 +1,9 @@
-public class MemoryCalc {
-}
+public class MemoryCalc extends Calculator {
+    double memoryValue = 0.0;
+
+    public void memoryAdd() {
+        memoryValue += currentValue;
+        System.out.println("Memory Add: " + String.format("%12.4f", memoryValue));
+    }
+
+   
