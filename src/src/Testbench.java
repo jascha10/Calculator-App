a6ac5c5336de4e3152b3cd84c7f3abab7b303c2b
ranran21-C <raniahassani2303@gmail.com>
2025-04-08 23:23:12 -0500

    add comments

--- src/src/Testbench.java
+++ src/src/Testbench.java
@@ -3,11 +3,16 @@ public class Testbench {
         System.out.println("Calculator On");
 
         AdvanceCalc calc = new AdvanceCalc();
-        calc.clear();
-        calc.add(10.22);          // Test normal add
-        calc.setPrecision(4);     // Set precision to 4 decimal places
-        calc.multiply(10.00);     // Test multiply with new precision
-        calc.sqrt(1600.00);       // Test square root with precision
-        calc.memoryAdd();         // Test memory functionality
+        calc.clear();               // Clear Calculator
+        calc.add(10.22);            // Add
+        calc.subtract(2.22);        // Subtract
+        calc.multiply(10.00);       // Multiply
+        calc.divide(2.00);          // Divide
+        calc.memoryAdd();           // Add to memory
+        calc.memorySubtract();      // Subtract from memory
+        calc.clearMemory();         // Clear memory
+
+        calc.setPrecision(4);       // Set precision to 4 decimal places
+        calc.sqrt(1600.00);         // Calculate square root
     }
 }
