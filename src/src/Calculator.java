d95482bc3579721d75189025623ef8c2c3350737
ranran21-C <raniahassani2303@gmail.com>
2025-04-08 23:04:36 -0500

    Update My Testbench to test multiply

--- src/src/Calculator.java
+++ src/src/Calculator.java
@@ -41,13 +41,15 @@ s public class Calculator {
     currentValue *= inputValue;
     updateDisplay();
 }
-   public class Testbench {
+  public class Testbench {
     public static void main(String[] args) {
         System.out.println("Calculator On");
 
         Calculator calc = new Calculator();
-        calc.clear();             
-        calc.add(10.22);          
-        calc.subtract(2.22);       
+        calc.clear();             // Output: 0.00
+        calc.add(10.22);          // 10.22
+        calc.subtract(2.22);      // 8.00
+        calc.multiply(10.00);     // 80.00
     }
+}
 }
